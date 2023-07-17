37d1f364e9fb0d0c46a9411ea6429e42502d089a
Diego L. Guarin <diego.guarinlopez@mail.mcgill.ca>
2023-07-17 10:13:09 -0400

    Initial Commit

---
+++ multiCameraView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multiCameraView
+//
+//  Created by DiegoGuarin on 7/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
